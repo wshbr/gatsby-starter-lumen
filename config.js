2fa23f7523d51f43a346cf23b925c3ffab63fcda
wshbr <Jesper@jspr.fritz.box>
2019-04-15 12:00:26 +0200

    Disqus-Update

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   title: 'Blog von Jesper',
   subtitle: 'Alles zu Lifestyle, Fashion und Blogging!',
   copyright: '© All rights reserved.',
-  disqusShortname: 'https-jspr-wshbr-de',
+  disqusShortname: 'jspr',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   menu: [
